849d1e8a5de7a5d8f05ea29b37771383c8d71005
Sourabh Kumar <sourabh99967780@gmail.com>
2020-10-31 22:30:53 +0530

    Static site deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,7 @@
 export default {
+  generate: {
+    routes: ["/posts/balut", "/posts/whereIsIt", "/posts/how"]
+  },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: "nuxt-fundamentals",
